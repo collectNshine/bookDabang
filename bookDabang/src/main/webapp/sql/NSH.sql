bbb95cf14e11f349f47876d4cf95872c3b14ad39
iamxx <iamxx@DESKTOP-9153F4B>
2023-06-20 17:46:14 +0900

    chat부분 sql 수정사항 commit

--- bookDabang/src/main/webapp/sql/NSH.sql
+++ bookDabang/src/main/webapp/sql/NSH.sql
@@ -1,21 +1,14 @@
--- 채팅참여
+-- 채팅방
 create table chat(
 	chat_num number not null,
-	mem_num number not null,
-	constraint chat_pk primary key (chat_num),
-	constraint chat_fk foreign key (mem_num) references member (mem_num),
-	constraint chat_fk2 foreign key (chat_num) references chat_list (chat_num)
-);
-
-create sequence chat_seq;
-
--- 채팅방
-create table chat_list(
-	chat_num number not null,
 	chat_title varchar2(50) not null,
+	chat_img varchar2(150),
+	hit number(9) default 0 not null,
 	reg_date date default sysdate not null,
 	constraint chatlist_pk primary key (chat_num)
 );
+
+create sequence chat_seq;
 
 -- 채팅 내용
 create table chat_message(
@@ -24,9 +17,8 @@ create table chat_message(
 	chat_num number not null,
 	chat_content clob not null,
 	chat_date date default sysdate not null,
-	constraint chatmessage_pk primary key (chat_num),
-	constraint chatmessage_fk foreign key (mem_num) references member (mem_num),
-	constraint chatmessage_fk2 foreign key (chat_num) references chat (chat_num)
+	constraint chatmessage_pk primary key (message_num),
+	constraint chatmessage_fk foreign key (chat_num) references chat (chat_num)
 );
 
 create sequence chat_message_seq;
