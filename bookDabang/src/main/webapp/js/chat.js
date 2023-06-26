41d7374c4a3622e1a4a9e2568d25f85573c5f9f4
iamxx <iamxx@DESKTOP-9153F4B>
2023-06-26 12:23:33 +0900

    담소마당 commit

--- bookDabang/src/main/webapp/js/chat.js
+++ bookDabang/src/main/webapp/js/chat.js
@@ -1,9 +1,33 @@
 $(function() {
 	let chat_num;
+	let message_socket = new WebSocket("ws://localhost:8080/bookDabang_sh/webSocket");
+	
+	message_socket.onopen = function(evt) {
+		console.log('open');
+		if(chat_num){ message_socket.send("usg:"); }
+	};
+	
+	//서버로부터 메시지를 받으면 호출되는 함수 지정
+	message_socket.onmessage = function(evt) {
+		//메시지 알림
+		let data = evt.data;
+		if (data.substring(0, 4) == "usg:") {
+			console.log('메시지 처리');
+			selectList();
+		}
+	};
+	
+	message_socket.onclose = function(evt) {
+		//소켓이 종료된 후 부과적인 작업이 있을 경우 명시
+		//console.log('chat close');
+		//alert('채팅이 종료되었습니다!');
+	};
+
 	// 채팅방 생성
 	$('#make_chat').on('click', function() {
 		$('.make-chat').show();
 		$('.top-bar,.bottm').empty();
+		$('.chatList-img').hide();
 		$('.chatRoom').hide();
 		
 		let make_chat = '';
@@ -114,9 +138,13 @@ $(function() {
 					}
 					
 					$('.chat-list').append(chatList);
+					$('.chat-list').scrollTop($('.chat-list')[0].scrollHeight)
 				});
 			},
-			error:function() { alert('Network Error!!'); }
+			error:function() {
+				alert('Network Error!!'); 
+				message_socket.close();
+			}
 		});
 	}
 	
@@ -125,6 +153,7 @@ $(function() {
 		$('.chatRoom').show();
 		$('.make-chat-content').empty();
 		$('.make-chat').hide();
+		$('.chatList-img').hide();
 		
 		let chatTitle = '<img src="../upload/' + $(this).attr('data-img') + '" class="chat-photo" width="40" height="40">';
 		if(!$(this).attr('data-img')) { chatTitle = '<img src="../images/face.png" class="chat-photo" width="40" height="40">'; }
@@ -135,27 +164,7 @@ $(function() {
 				
 		selectList();
 	});
-	
-	// 채팅 등록
-	function insertChat() {
-		let chat_content = $('.chat-content').val();
-		
-		$.ajax({
-			url:'writeChat.do',
-			type:'post',
-			data:{chat_content:chat_content, chat_num:chat_num},
-			dataType:'json',
-			success:function(param) {
-				if(param.result == 'logout') { alert('Login 후 작성 가능!!'); }
-				else if(param.result == 'success') { 
-					$('.chat-content').val(''); 
-					selectList();
-				} else { alert('채팅을 전송할 수 없습니다.'); }
-			},
-			error:function() { alert('Network Error!!'); }
-		});
-	}
-	
+
 	// click
 	$(document).on('submit', '.write-chat', function(event) {
 		event.preventDefault();
@@ -166,9 +175,41 @@ $(function() {
 			return false;
 		}
 		
-		insertChat();
+		let chat_content = $('.chat-content').val();
+		
+		$.ajax({
+			url:'writeChat.do',
+			type:'post',
+			data:{chat_content:chat_content, chat_num:chat_num},
+			dataType:'json',
+			success:function(param) {
+				if(param.result == 'logout') { 
+					alert('Login 후 작성 가능!!'); 
+					message_socket.close();
+				}
+				else if(param.result == 'success') { 
+					$('.chat-content').val(''); 
+					message_socket.send('usg:');
+				} else { 
+					alert('채팅을 전송할 수 없습니다.'); 
+					message_socket.close();
+				}
+			},
+			error:function() { 
+				alert('Network Error!!'); 
+				message_socket.close();	
+			}
+		});
 	});
 	
+	// Enter 동작
+	$('.chat-content').keydown(function(event) {
+		if(event.keyCode == 13 && !event.shiftKey){
+			$(".write-chat").trigger("submit");
+		}
+	});
+	
+	/*
 	// enter
 	$(document).on('keypress', '.chat-content', function(event) {
 		
@@ -177,8 +218,8 @@ $(function() {
 			return false;
 		}
 		
-		if(event.keyCode && evnet.keyCode == 13){
-			  e.preventDefault();	
+		if(event.keyCode && event.keyCode == 13){
+			  return;	
 		}
 		
 		if($('.chat-content').val().trim() == '') {
@@ -189,6 +230,7 @@ $(function() {
 		
 		insertChat();
 	});
+	*/
 	
 	// 닫기버튼2	
 	$('#out_chat2').click(function() {
